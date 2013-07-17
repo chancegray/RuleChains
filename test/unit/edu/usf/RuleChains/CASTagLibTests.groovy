0f88bd7b13b73868e74d66fe82e0c63e3b92ba67
James Jones <jamjon3@gmail.com>
2013-07-17 14:09:15 -0400

    A CAS Spring Security TagLib for display functions

---
+++ test/unit/edu/usf/RuleChains/CASTagLibTests.groovy
@@ -0,0 +1,17 @@
+package edu.usf.RuleChains
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(CASTagLib)
+class CASTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
