7fc1927146b7cd433f8a1c0e95caa81961017930
James Jones <jamjon3@gmail.com>
2013-08-08 15:15:26 -0400

    #14 Added a JSON function to handle refreshing the running jobs list service

--- web-app/js/jquery.ruleChains.js
+++ web-app/js/jquery.ruleChains.js
@@ -266,6 +266,33 @@
                     }
                 });        
             },
+            // listCurrentlyExecutingJobs
+            GETlistCurrentlyExecutingJobs: function(json,callback) {
+                json = jQuery.extend({
+                    name: ""
+                },json);
+                $.ajax({
+                    url: "/RuleChains/running/",
+                    type: "GET",
+                    dataType : "json",
+                    beforeSend: function (XMLHttpRequest, settings) {
+                        XMLHttpRequest.setRequestHeader("Content-Type", "application/json");
+                        XMLHttpRequest.setRequestHeader("Accept", "application/json");
+                    },
+//                    data: ($.trim(json.pattern).length < 1)?{}:{
+//                        pattern: $.trim(json.pattern)
+//                    },
+                    success: callback,
+                    error: function (jqXHR,  textStatus, errorThrown) {
+                        if (jqXHR.status === 0) {
+                            // Session has probably expired and needs to reload and let CAS take care of the rest
+                            alert('Your session has expired, the page will need to reload and you may be asked to log back in');
+                            // reload entire page - this leads to login page
+                            window.location.reload();
+                        }
+                    }
+                });        
+            },
             POSTmergescheduleChainJob: function(json,callback) {
                 json = jQuery.extend(true,{
                     name: "",
