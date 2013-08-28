6ee63402be1053a3bcc145547d437b11a4d4cdb1
James Jones <jamjon3@gmail.com>
2013-08-28 11:32:37 -0400

    Updated the "sample" config file to include setting up the application urls per environment and CAS configuration per environment as well. Also "renamed" the example file.

--- Samples/RuleChainsConfig.groovy
+++ Samples/RuleChainsConfig.groovy
@@ -31,3 +31,34 @@ grails.plugins.springsecurity.interceptUrlMap = [
     // Definable services tied to a rule chain
     '/service/testServicehandler/': ["hasRole('ROLE_SOME_ROLE_FOR_DEFINEDSERVICE')"]
 ]
+environments {
+    production {
+        grails.serverURL = "http://somedomain.org:8080/RuleChains"
+        grails.plugins.springsecurity.cas.serverUrlPrefix = 'https://authtest.it.usf.edu'
+    }
+    development {
+        grails.serverURL = "http://localhost:8080/RuleChains"
+        grails.plugins.springsecurity.cas.serverUrlPrefix = 'https://authtest.it.usf.edu'
+    }
+    test {
+        grails.serverURL = "http://localhost:8080/RuleChains"
+        grails.plugins.springsecurity.cas.serverUrlPrefix = 'https://authtest.it.usf.edu'
+    }
+}
+
+// Added by the Spring Security CAS (USF) plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'edu.usf.cims.UsfCasUser'
+grails.plugins.springsecurity.cas.active = true
+grails.plugins.springsecurity.cas.sendRenew = false
+grails.plugins.springsecurity.cas.key = '9a3433aca7184008df30ee8f5c62f160' //unique value for each app
+grails.plugins.springsecurity.cas.artifactParameter = 'ticket'
+grails.plugins.springsecurity.cas.serviceParameter = 'service'
+grails.plugins.springsecurity.cas.filterProcessesUrl = '/j_spring_cas_security_check'
+grails.plugins.springsecurity.cas.proxyCallbackUrl = "${grails.serverURL}/secure/receptor"
+grails.plugins.springsecurity.cas.proxyReceptorUrl = '/secure/receptor'
+grails.plugins.springsecurity.cas.useSingleSignout = false
+grails.plugins.springsecurity.cas.driftTolerance = 120000
+grails.plugins.springsecurity.cas.loginUri = '/login'
+grails.plugins.springsecurity.cas.useSamlValidator = true
+grails.plugins.springsecurity.cas.authorityAttribute = 'eduPersonEntitlement'
+grails.plugins.springsecurity.cas.serviceUrl = "${grails.serverURL}/j_spring_cas_security_check"
