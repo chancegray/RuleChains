a4df226117746555b5a867e7390d47133e2bd552
James Jones <jamjon3@gmail.com>
2013-09-17 16:25:19 -0400

    Fixed the JobHistory by letting lines go to rows of a JobLog table

--- grails-app/domain/edu/usf/RuleChains/JobHistory.groovy
+++ grails-app/domain/edu/usf/RuleChains/JobHistory.groovy
@@ -8,7 +8,7 @@ class JobHistory {
     String cron = ""
     Date fireTime
     Date scheduledFireTime
-    String jobLog = ""
+    static hasMany = [jobLogs:JobLog]
     static constraints = {
          name(   
              blank: false,
@@ -22,30 +22,10 @@ class JobHistory {
          cron(blank:true)
          fireTime(nullable:true)
          scheduledFireTime(nullable:true)
-         jobLog(blank:true)
     }
-    static mapping = {
-        jobLog type: 'text'
-    }  
     def appendToLog(String logtext) {
-        jobLog = (!!jobLog)?[jobLog,logtext].join("\n"):logtext
-        if(!this.save(failOnError:false, flush: true, insert: false, validate: true)) {
+        if(!this.addToJobLogs(new JobLog(line: logtext)).save(failOnError:false,flush:true,validate:true)) {
             log.error "'${this.errors.fieldError.field}' value '${this.errors.fieldError.rejectedValue}' rejected" 
-            def s = ""
-            new StringReader( jobLog ).withReader { r ->
-                // Drop the lines to make space for the new log entry
-                def size = 0
-                while(size < (logtext.size()+1)) {
-                    size += r.readLine().size()+1
-                }
-                while( ( line = r.readLine() ) != null ) {
-                    s = (!!s)?[s,line].join("\n"):line
-                }
-            }
-            jobLog = s                
-            if(!this.save(failOnError:false, flush: true, insert: false, validate: true)) {
-                log.error "'${this.errors.fieldError.field}' value '${this.errors.fieldError.rejectedValue}' rejected after reducing the first line" 
-            }
         }
     }
     def updateJobProperties(def jobCtx) {
