edf38eab16956df395f65008368284e09abdba46
James Jones <jamjon3@gmail.com>
2013-09-18 15:45:04 -0400

    Created a Job Tag Library for use in the GSP display of Job Histories

---
+++ grails-app/taglib/edu/usf/RuleChains/JobTagLib.groovy
@@ -0,0 +1,11 @@
+package edu.usf.RuleChains
+
+class JobTagLib {
+    static namespace = "job"  
+    def jobService
+    def jobHistorySelect = {
+        out << '<label for="jobHistories" style="padding-right:15px;">Select Job History To View</label>'
+        out << g.select(name:"jobHistory",from:jobService.getJobHistories().jobHistories.sort{ it.name },optionKey:"id",optionValue:"name",noSelection:['':'--ALL--'])
+    }
+
+}
