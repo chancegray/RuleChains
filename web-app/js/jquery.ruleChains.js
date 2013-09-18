93432b93b1286b7c7d87bf6587515ff244662bd5
James Jones <jamjon3@gmail.com>
2013-09-18 14:39:26 -0400

    Defined an AJAX method for deleteJobHistory DELETE service

--- web-app/js/jquery.ruleChains.js
+++ web-app/js/jquery.ruleChains.js
@@ -534,6 +534,30 @@
                         }
                     }
                 });                        
+            },
+            DELETEdeleteJobHistory: function(json,callback) {
+                json = jQuery.extend(true,{
+                    name: ""
+                },json);
+                $.ajax({
+                    url: '/RuleChains/history/'+json.name,
+                    type: "DELETE",
+                    dataType : "json",
+                    beforeSend: function (XMLHttpRequest, settings) {
+                        XMLHttpRequest.setRequestHeader("Content-Type", "application/json");
+                        XMLHttpRequest.setRequestHeader("Accept", "application/json");
+                    },
+                    // data: JSON.stringify(json),
+                    success: callback,
+                    error: function (jqXHR,  textStatus, errorThrown) {
+                        if (jqXHR.status === 0) {
+                            // Session has probably expired and needs to reload and let CAS take care of the rest
+                            alert('Your session has expired, the page will need to reload and you may be asked to log back in');
+                            // reload entire page - this leads to login page
+                            window.location.reload();
+                        }
+                    }
+                });                                                                
             }
         },
         chain: {
