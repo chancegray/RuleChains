6d787a27c4b1445e0116929602763ab1e4e8b681
James Jones <jamjon3@gmail.com>
2013-12-04 14:16:19 -0500

    #47 Added before and after methods to sync with the git rule repository

--- grails-app/domain/edu/usf/RuleChains/StoredProcedureQuery.groovy
+++ grails-app/domain/edu/usf/RuleChains/StoredProcedureQuery.groovy
@@ -16,4 +16,18 @@ class StoredProcedureQuery extends Rule {
     static mapping = {
         rule type: 'text'
     }    
+    
+    def afterInsert() {
+        saveGitWithComment("Creating ${name} StoredProcedureQuery")
+    }
+    def beforeUpdate() {
+        updateGitWithComment("Renaming ${name} StoredProcedureQuery")
+    }
+    def afterUpdate() {
+        saveGitWithComment("Updating ${name} StoredProcedureQuery")
+    }
+    def beforeDelete() {
+        deleteGitWithComment("Deleted ${name} StoredProcedureQuery")
+    }
+    
 }
