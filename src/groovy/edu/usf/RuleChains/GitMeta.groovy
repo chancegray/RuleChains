c52e8840b4704145f82acf0805ad1ea8c9d239d0
James Jones <jamjon3@gmail.com>
2013-12-04 14:26:57 -0500

    #47 Added all the meta methods for all the "before" and "after" methods on Rule and RuleSet domain classes to facilitate the git repository sync.

--- src/groovy/edu/usf/RuleChains/GitMeta.groovy
+++ src/groovy/edu/usf/RuleChains/GitMeta.groovy
@@ -10,6 +10,9 @@ import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.InitCommand
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.lib.Repository
+import grails.converters.*
+import edu.usf.RuleChains.*
+import org.hibernate.FlushMode
 
 /**
  *
@@ -18,9 +21,9 @@ import org.eclipse.jgit.lib.Repository
 
 class GitMeta {
     def gitRepository = null
-    def buildMeta = { baseFolder ->
+    def buildMeta = { grailsApplication ->
         def command = Git.init()
-        command.directory = new File(baseFolder + '/git/')
+        command.directory = new File(grailsApplication.mainContext.getResource('/').file.absolutePath + '/git/')
 
         def repository
         
@@ -39,8 +42,171 @@ class GitMeta {
         git.commit().setMessage("Initial commit of RuleChains code sources.").call()
         println "Committed initial code to the git repository."
 
-        gitRepository = repository       
-        
+        gitRepository = repository  
+        def metaClasses = []
+        for (domainClass in grailsApplication.domainClasses.findAll { sc -> sc.name in ['DefinedService','SQLQuery','Snippet','StoredProcedureQuery','PHP','Groovy']}) {
+            domainClass.metaClass.deleteGitWithComment {comment ->
+                new File("${command.directory.absolutePath}/ruleSets/${delegate.getPersistentValue('ruleSet').name}/${delegate.name}.json").delete()
+                git.commit().setMessage(comment).call()
+            }
+            domainClass.metaClass.updateGitWithComment {comment ->
+                if (delegate.isDirty('name')) {
+                    def f = new File("${command.directory.absolutePath}/ruleSets/${delegate.ruleSet.name}/${delegate.getPersistentValue("name")}.json")
+                    if(f.exists()) {
+                        f.renameTo(new File("${command.directory.absolutePath}/ruleSets/${delegate.ruleSet.name}/${delegate.name}.json"))
+                        git.commit().setMessage(comment).call()
+                    }
+                }
+            }
+            domainClass.metaClass.saveGitWithComment {comment ->
+                def f = new File("${command.directory.absolutePath}/ruleSets/${delegate.ruleSet.name}/${delegate.name}.json")
+                switch(delegate) {
+                    case { it instanceof SQLQuery }:
+                        println "Writing SQLQuery File ${delegate.name}.json"
+                        f.text = {j->
+                            j.setPrettyPrint(true)
+                            return j
+                        }.call([
+                            name: delegate.name,
+                            rule: delegate?.rule,
+                            "class": delegate['class']
+                        ] as JSON)                        
+                        break
+                    case { it instanceof Groovy }:
+                        println "Writing Groovy File ${delegate.name}.json"
+                        f.text = {j->
+                            j.setPrettyPrint(true)
+                            return j
+                        }.call([
+                            name: delegate.name,
+                            rule: delegate?.rule,
+                            "class": delegate['class']
+                        ] as JSON)                        
+                        break
+                    case { it instanceof PHP }:
+                        println "Writing PHP File ${delegate.name}.json"
+                        f.text = {j->
+                            j.setPrettyPrint(true)
+                            return j
+                        }.call([
+                            name: delegate.name,
+                            rule: delegate?.rule,
+                            "class": delegate['class']
+                        ] as JSON)                        
+                        break
+                    case { it instanceof StoredProcedureQuery }:
+                        println "Writing StoredProcedureQuery File ${delegate.name}.json"
+                        f.text = {j->
+                            j.setPrettyPrint(true)
+                            return j
+                        }.call([
+                            name: delegate.name,
+                            rule: delegate?.rule,
+                            "class": delegate['class'],
+                            "closure": delegate['closure']
+                        ] as JSON)                        
+                        break
+                    case { it instanceof DefinedService }:  
+                        println "Writing DefinedService File ${delegate.name}.json"
+                        def ds = delegate as JSON
+                        println ds
+                        f.text = {j->
+                            j.setPrettyPrint(true)
+                            return j
+                        }.call([
+                            name: delegate.name,
+                            method:{ m->
+                                switch(m) {
+                                    case MethodEnum.GET:
+                                        return "GET"
+                                        break
+                                    case MethodEnum.POST:
+                                        return "POST"
+                                        break
+                                    case MethodEnum.PUT:
+                                        return "PUT"
+                                        break
+                                    case MethodEnum.DELETE:
+                                        return "DELETE"
+                                        break
+                                }                                
+                            }.call(delegate.method),
+                            authType:{ t->
+                                switch(t) {
+                                    case AuthTypeEnum.NONE:
+                                        return "NONE"
+                                        break
+                                    case AuthTypeEnum.BASIC:
+                                        return "BASIC"
+                                        break
+                                    case AuthTypeEnum.DIGEST:
+                                        return "DIGEST"
+                                        break
+                                    case AuthTypeEnum.CAS:
+                                        return "CAS"
+                                        break
+                                    case AuthTypeEnum.CASSPRING:
+                                        return "CASSPRING"
+                                        break
+                                }                                
+                            }.call(delegate.authType),
+                            parse:{ p->
+                                switch(p) {
+                                    case ParseEnum.TEXT:
+                                        return "TEXT"
+                                        break
+                                    case ParseEnum.XML:
+                                        return "XML"
+                                        break
+                                    case ParseEnum.JSON:
+                                        return "JSON"
+                                        break
+                                }                                
+                            }.call(delegate.parse),
+                            url: delegate.url,
+                            springSecurityBaseURL: delegate.springSecurityBaseURL,
+                            user: delegate.user,
+                            password: delegate.password,
+                            "class": delegate['class']
+                        ] as JSON)    
+                        println "DONE!"
+                        break
+                    case { it instanceof Snippet }:
+                        println "Writing Snippet File ${delegate.name}.json"
+                        f.text = {j->
+                            j.setPrettyPrint(true)
+                            return j
+                        }.call([
+                            name: delegate.name,
+                            chain: delegate?.chain?.name,
+                            "class": delegate['class']
+                        ] as JSON)                        
+                        break
+                }
+                git.commit().setMessage(comment).call()
+            }       
+        }
+        RuleSet.metaClass.deleteGitWithComment  = {comment->  
+            new File("${command.directory.absolutePath}/ruleSets/${delegate.name}/").deleteDir()
+            git.commit().setMessage(comment).call()
+        }
+        RuleSet.metaClass.updateGitWithComment = {comment ->
+            def of = new File("${command.directory.absolutePath}/ruleSets/${delegate.getPersistentValue("name")}/")
+            def f = new File("${command.directory.absolutePath}/ruleSets/${delegate.name}/")
+            if(of.exists()) {
+                of.renameTo(f)
+            } else if(!f.exists()) {
+                f.mkdirs()
+            }
+            git.commit().setMessage(comment).call()
+        }
+        RuleSet.metaClass.saveGitWithComment = {comment ->
+            def f = new File("${command.directory.absolutePath}/ruleSets/${delegate.name}/")
+            if(!f.exists()) {
+                f.mkdirs()
+            }            
+            git.commit().setMessage(comment).call()
+        }
         
     }
 }
