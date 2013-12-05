b648c3ae425faf5c23b96865c75bfdf3c831617f
James Jones <jamjon3@gmail.com>
2013-12-05 08:51:52 -0500

    #47 Modified afterInsert method to update matching links in the git repo (so there won't be orphaned rule names referenced when the database reference is updated)

--- grails-app/domain/edu/usf/RuleChains/SQLQuery.groovy
+++ grails-app/domain/edu/usf/RuleChains/SQLQuery.groovy
@@ -15,6 +15,12 @@ class SQLQuery extends Rule {
     }
     def afterUpdate() {
         saveGitWithComment("Updating ${name} SQLQuery")
+        /**
+         * Anytime a rule is renamed, any link referenced rule name in git repo needs to be updated (if exists)
+         **/
+        Link.findAllByRule(this).each { l ->
+            l.saveGitWithComment("Updating Link referencing ${name} SQLQuery")
+        }
     }
     def beforeDelete() {
         deleteGitWithComment("Deleted ${name} SQLQuery")
