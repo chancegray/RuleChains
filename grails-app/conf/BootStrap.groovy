ed40c75ecf216586bd1ee6b91da22aa25d40c405
James Jones <jamjon3@gmail.com>
2013-12-18 15:25:37 -0500

    #47 More schedule wiring for Git

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,6 +6,7 @@ import edu.usf.RuleChains.JobService
 import edu.usf.RuleChains.JobController
 import edu.usf.RuleChains.LinkService
 import edu.usf.RuleChains.RuleChainsJobListener
+import edu.usf.RuleChains.RuleChainsSchedulerListener
 import edu.usf.RuleChains.SQLQuery
 import edu.usf.RuleChains.RuleSet
 import edu.usf.RuleChains.Link
@@ -14,6 +15,7 @@ import edu.usf.RuleChains.JobMeta
 import edu.usf.RuleChains.GitMeta
 import edu.usf.RuleChains.Groovy
 import org.quartz.JobListener
+import org.quartz.listeners.SchedulerListenerSupport
 
 class BootStrap {
     def grailsApplication
@@ -35,6 +37,7 @@ class BootStrap {
             gitMeta.buildMeta(grailsApplication,usfCasService)
             // Added Job Listener for Git Sync'ing
             quartzScheduler.getListenerManager().addJobListener(new RuleChainsJobListener() as JobListener)
+            quartzScheduler.getListenerManager().addSchedulerListener(new RuleChainsSchedulerListener())
             print jobService.listChainJobs()
             configService.syncronizeDatabaseFromGit()
         }
