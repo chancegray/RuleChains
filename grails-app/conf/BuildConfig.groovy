745bd1c567c1546dd5052be12a2fcbd5932bde57
James Jones <jamjon3@gmail.com>
2014-01-14 11:19:44 -0500

    #46 Upgraded the CAS client which solved the issue

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
         compile ':spring-security-core:1.2.7.3'
         compile ":spring-security-acl:1.1.1"
         compile ':spring-security-cas-usf:1.2.1'
-        compile ':grails-cas-rest-client:0.3'
+        compile ':grails-cas-rest-client:0.3.1'
         compile ":quartz2:2.1.6.2"
         compile ":rest:0.7"
     }
