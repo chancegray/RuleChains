6c4b6feda7c2c498e5549ab3d76f7442d7a44cb8
James Jones <jamjon3@gmail.com>
2014-01-14 13:05:07 -0500

    #41 Added the missing jobHistory into the handled chain

--- grails-app/controllers/edu/usf/RuleChains/ChainServiceHandlerController.groovy
+++ grails-app/controllers/edu/usf/RuleChains/ChainServiceHandlerController.groovy
@@ -9,7 +9,7 @@ class ChainServiceHandlerController {
     def handleChainService() {
         withFormat {
             html {
-                return chainServiceHandlerService.handleChainService(params.handler,request.method,params.input)
+                JSON.use("deep") { render chainServiceHandlerService.handleChainService(params.handler,request.method,params.input) as JSON }
             }
             xml {
                 render chainServiceHandlerService.handleChainService(params.handler,request.method,params.input) as XML
