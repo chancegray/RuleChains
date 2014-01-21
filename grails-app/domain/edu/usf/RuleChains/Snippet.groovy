51f94f05f50af511e6f7e7aebb1ba5edc916ec65
James Jones <jamjon3@gmail.com>
2014-01-21 11:19:40 -0500

    Added some comments

--- grails-app/domain/edu/usf/RuleChains/Snippet.groovy
+++ grails-app/domain/edu/usf/RuleChains/Snippet.groovy
@@ -1,21 +1,37 @@
 package edu.usf.RuleChains
 
+/**
+ * Snippet extends the basic Rule domain class and is the unit
+ * for processing a rule which executes an embedded rule chain.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
 class Snippet extends Rule {
     Chain chain
     static constraints = {
         chain(nullable:true)
     }
-    
+    /*
+     * Handles syncronization for saves 
+     */    
     def afterInsert() {
         if(isSynced) {
             saveGitWithComment("Creating ${name} Snippet")
         }
     }
+    /*
+     * Handles syncronization for update
+     */    
     def beforeUpdate() {
         if(isSynced) {
             updateGitWithComment("Renaming ${name} Snippet")
         }
     }
+    /*
+     * Handles syncronization for post-update saves 
+     */        
     def afterUpdate() {
         if(isSynced) {
             saveGitWithComment("Updating ${name} Snippet")
@@ -27,6 +43,9 @@ class Snippet extends Rule {
             }        
         }
     }
+    /*
+     * Handles syncronization for deletes 
+     */        
     def beforeDelete() {
         if(isSynced) {
             deleteGitWithComment("Deleted ${name} Snippet")
