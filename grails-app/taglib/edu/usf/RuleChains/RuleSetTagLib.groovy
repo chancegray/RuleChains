9e7a5f8c439b604644021d2a8dad27a50edc2515
James Jones <jamjon3@gmail.com>
2014-01-21 08:35:19 -0500

    Added some comments

--- grails-app/taglib/edu/usf/RuleChains/RuleSetTagLib.groovy
+++ grails-app/taglib/edu/usf/RuleChains/RuleSetTagLib.groovy
@@ -1,8 +1,21 @@
 package edu.usf.RuleChains
 
+/**
+ * RuleSetTagLib is a taglib for RuleSet GSP views.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
 class RuleSetTagLib {
     static namespace = "ruleSet"  
     def ruleSetService
+    
+    /**
+     * Provides a GSP method to produce a HTML select element of available rulesets.
+     * 
+     * @return A list of available rulesets formatted in an HTML select element
+     */         
     def ruleSetSelect = {
         out << '<label for="ruleSet" style="padding-right:15px;">Select Rule Set To View</label>'
         out << g.select(name:"ruleSet",from:ruleSetService.listRuleSets().ruleSets.sort{ it.name },optionKey:"id",optionValue:"name",noSelection:['':'--ALL--'])
