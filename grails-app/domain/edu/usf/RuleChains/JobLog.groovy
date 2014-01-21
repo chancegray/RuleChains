0486eec2d74b01e2e58f321d6d26885f887c919f
James Jones <jamjon3@gmail.com>
2014-01-21 12:20:37 -0500

    Added some comments

--- grails-app/domain/edu/usf/RuleChains/JobLog.groovy
+++ grails-app/domain/edu/usf/RuleChains/JobLog.groovy
@@ -1,5 +1,12 @@
 package edu.usf.RuleChains
 
+/**
+ * JobLog domain class stores JobHistory entries related to an executing rule chain job.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
 class JobLog {
     Date logTime = new Date()
     String line
