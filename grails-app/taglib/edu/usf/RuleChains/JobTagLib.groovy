99b5f1c67b5d957bd0c27cebf49c0a1b8a072622
James Jones <jamjon3@gmail.com>
2014-01-22 12:55:35 -0500

    Added some comments

--- grails-app/taglib/edu/usf/RuleChains/JobTagLib.groovy
+++ grails-app/taglib/edu/usf/RuleChains/JobTagLib.groovy
@@ -1,8 +1,20 @@
 package edu.usf.RuleChains
 
+/**
+ * JobTagLib is a taglib for Quart Job GSP views.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
 class JobTagLib {
     static namespace = "job"  
     def jobService
+    /**
+     * Provides a GSP method to produce a HTML select element of available job histories.
+     * 
+     * @return A list of available job histories formatted in an HTML select element
+     */         
     def jobHistorySelect = {
         out << '<label for="jobHistories" style="padding-right:15px;">Select Job History To View</label>'
         out << g.select(name:"jobHistory",from:jobService.getJobHistories().jobHistories.sort{ it.name },optionKey:"name",optionValue:"name",noSelection:['':'--ALL--'])
