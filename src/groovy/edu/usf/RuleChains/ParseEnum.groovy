239de755e080322ba311152560d0da137c78e2d5
James Jones <jamjon3@gmail.com>
2014-01-23 07:59:58 -0500

    Added some comments

--- src/groovy/edu/usf/RuleChains/ParseEnum.groovy
+++ src/groovy/edu/usf/RuleChains/ParseEnum.groovy
@@ -6,11 +6,20 @@
 package edu.usf.RuleChains
 
 /**
- *
- * @author james
- */
+ * ParseEnum is an Enumerator for output Parsing Types.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
 enum ParseEnum {
     TEXT, XML, JSON;
+    /**
+     * Converts a base string into a corresponding enumerated type
+     * 
+     * @param   str    The string name of the enumerated type
+     * @return         A corresponding enumerated type matched on the string parameter
+     */            
     public static ParseEnum byName(String str) {
         for (parseEnum in ParseEnum.values()) {
             if(str.trim().toUpperCase().equalsIgnoreCase(parseEnum.name())) {
