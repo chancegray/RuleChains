860edde069a90f6617b8f4c03f2a81b5c525a860
James Jones <jamjon3@gmail.com>
2014-01-24 08:55:10 -0500

    Added some comments

--- src/groovy/edu/usf/RuleChains/LinkMeta.groovy
+++ src/groovy/edu/usf/RuleChains/LinkMeta.groovy
@@ -7,13 +7,27 @@ package edu.usf.RuleChains
 import edu.usf.RuleChains.LinkService
 import groovy.sql.Sql
 /**
- *
- * @author james
- */
-
-
+ * LinkMeta performs all the metaprogramming for the LinkService
+ * mainly to handle Groovy SQL and Hibernate sessions
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
 class LinkMeta {
+    /**
+     * The builder method that creates all the metaprogramming methods
+     * 
+     * @param   grailsApplication    The Grails GrailsApplication object
+     */
     def buildMeta = {grailsApplication->
+        /**
+         * Retrives a hibernate session from the session factory based on a provided name match.
+         * 
+         * @param     name     The name of the datasource (when using multiple datasources)
+         * @return             A hibernate session
+         * @see       Session
+         */
         LinkService.metaClass.getSourceSession { String name ->
             String sfRoot = "sessionFactory_"
             def sfb = grailsApplication.mainContext.beanDefinitionNames.findAll{ it.startsWith( 'sessionFactory_' ) }.find{ it.endsWith(name) }
@@ -22,6 +36,13 @@ class LinkMeta {
             }
             return grailsApplication.mainContext."sessionFactory".currentSession
         }     
+        /**
+         * Retrives a Groovy SQL object from the session factory based on a provided name match.
+         * 
+         * @param     name     The name of the datasource (when using multiple datasources)
+         * @return             A Groovy SQL object
+         * @see       Sql
+         */
         LinkService.metaClass.getSQLSource { String name ->
             String sfRoot = "sessionFactory_"
             def sfb = grailsApplication.mainContext.beanDefinitionNames.findAll{ it.startsWith( 'sessionFactory_' ) }.find{ it.endsWith(name) }
@@ -30,6 +51,12 @@ class LinkMeta {
             }
             return new Sql(grailsApplication.mainContext."sessionFactory".currentSession.connection())                
         }
+        /**
+         * Retrives hashmap of Groovy SQL objects derrived from the available session factories
+         * 
+         * @return             A hashmap of Groovy SQL objects
+         * @see       Sql
+         */
         LinkService.metaClass.getSQLSources {
             String sfRoot = "sessionFactory_"
             def sfb = grailsApplication.mainContext.beanDefinitionNames.findAll{ it.startsWith( 'sessionFactory_' ) }.collectEntries { b ->
