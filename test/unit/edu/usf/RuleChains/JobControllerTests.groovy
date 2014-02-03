74d156807573c9912705d1b2038e76acfcb99dc9
James Jones <jamjon3@gmail.com>
2014-02-03 15:21:53 -0500

    #58 Adding JobController tests

--- test/unit/edu/usf/RuleChains/JobControllerTests.groovy
+++ test/unit/edu/usf/RuleChains/JobControllerTests.groovy
@@ -9,9 +9,56 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(JobController)
+@Mock([JobService])
 class JobControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testListChainJobs() {
+        controller.request.method = "GET"
+        def control = mockFor(JobService)
+        control.demand.listChainJobs { -> 
+            return [
+                jobGroups: [
+                    [
+                        name: 'default',
+                        jobs: [
+                            [
+                                name: 'test1',
+                                triggers: '0 0 0 0 ? 2014',
+                                chain: 'testchain',
+                                input: []
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        }
+        controller.jobService = control.createMock()
+
+        controller.request.contentType = "text/json"
+        // controller.request.content = (["pattern": null] as JSON).toString().getBytes()
+        def model = controller.listChainJobs()
+        assert model.jobGroups[0].name == "default"
+    }
+    
+    void testCreateChainJob() {
+        controller.params << [
+            cronExpression: "0 0 0 0 ? 2015",
+            name: "newChainJob",
+            input: []
+        ]
+        controller.request.method = "PUT"
+        JobService.metaClass.createChainJob = { String cronExpression,String name,def input = [] -> }
+        def control = mockFor(JobService)
+        control.demand.createChainJob { cronExpression,name,input -> 
+            return [
+                date: System.currentTimeMillis()
+            ]
+        }
+        controller.jobService = control.createMock()
+        
+        controller.request.contentType = "text/json"
+        // controller.request.content = (["pattern": null] as JSON).toString().getBytes()
+        def model = controller.createChainJob()
+        assert model.date <= System.currentTimeMillis()        
     }
 }
