d0df2b40a45ff1b27e2a84935c4c835be619c8fd
James Jones <jamjon3@gmail.com>
2014-02-06 14:27:59 -0500

    #58 Adding RuleSetController tests

--- test/unit/edu/usf/RuleChains/RuleSetControllerTests.groovy
+++ test/unit/edu/usf/RuleChains/RuleSetControllerTests.groovy
@@ -4,14 +4,67 @@ package edu.usf.RuleChains
 
 import grails.test.mixin.*
 import org.junit.*
-
+import java.util.regex.*
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(RuleSetController)
+@Mock([RuleSetService,Rule,SQLQuery,RuleSet])
 class RuleSetControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testListRuleSets() {
+        controller.params.pattern = null        
+        controller.request.method = "GET"
+        def control = mockFor(RuleSetService)
+
+        control.demand.listRuleSets { pattern -> 
+            def rsObj = [
+                "ruleSets": [
+                    new RuleSet(name: "firstRuleSet"),
+                    new RuleSet(name: "secondRuleSet")
+                ]
+            ] 
+            if(pattern) {
+                return [ruleSets: rsObj.ruleSets.findAll {
+                    Pattern.compile(pattern.trim()).matcher(it.name).matches()
+                }]
+            } else {
+                return rsObj
+            }
+        }
+        controller.ruleSetService = control.createMock()
+
+        controller.request.contentType = "text/json"
+        // controller.request.content = (["pattern": null] as JSON).toString().getBytes()
+        def model = controller.listRuleSets()
+        assert model.ruleSets[0].name == "firstRuleSet"        
+    }
+
+    void testListRuleSetsPattern() {
+        controller.params.pattern = "^(second).*"        
+        controller.request.method = "GET"
+        def control = mockFor(RuleSetService)
+
+        control.demand.listRuleSets { pattern -> 
+            def rsObj = [
+                "ruleSets": [
+                    new RuleSet(name: "firstRuleSet"),
+                    new RuleSet(name: "secondRuleSet")
+                ]
+            ] 
+            if(pattern) {
+                return [ruleSets: rsObj.ruleSets.findAll {
+                    Pattern.compile(pattern.trim()).matcher(it.name).matches()
+                }]
+            } else {
+                return rsObj
+            }
+        }
+        controller.ruleSetService = control.createMock()
+
+        controller.request.contentType = "text/json"
+        // controller.request.content = (["pattern": null] as JSON).toString().getBytes()
+        def model = controller.listRuleSets()
+        assert model.ruleSets[0].name == "secondRuleSet"        
     }
 }
