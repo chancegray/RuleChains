ab857cd0b779aa2b6f2aa2486c8ea2f30b5c2104
James Jones <jamjon3@gmail.com>
2014-02-12 15:41:25 -0500

    #58 Added some comments

--- test/unit/edu/usf/RuleChains/RuleSetServiceTests.groovy
+++ test/unit/edu/usf/RuleChains/RuleSetServiceTests.groovy
@@ -6,12 +6,21 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ * RuleSetServiceTests provides for unit testing of services handling the processing and manipulation of RuleSet objects
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(RuleSetService)
 @Mock([Rule,SQLQuery,RuleSet,ChainServiceHandler,Chain,Link])
 class RuleSetServiceTests {
-
+    /**
+     * Tests the returns of a list of RuleSet objects objects without a matching filter
+     * 
+     */ 
     void testListRuleSets() {
         def ruleSetService = new RuleSetService()
         [
@@ -26,7 +35,10 @@ class RuleSetServiceTests {
         assert result.ruleSets.find { it.name == "firstRuleSet" }.name == "firstRuleSet"
         assert result.ruleSets.find { it.name == "secondRuleSet" }.name == "secondRuleSet"
     }
-
+    /**
+     * Tests the returns of a list of RuleSet objects objects with a matching filter
+     * 
+     */ 
     void testListRuleSetsPattern() {
         def ruleSetService = new RuleSetService()
         [
