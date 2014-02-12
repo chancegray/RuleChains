32720e582a5d1cafd03c46b04fd1f64d4f6aea15
James Jones <jamjon3@gmail.com>
2014-02-12 15:28:08 -0500

    #58 Adding RuleSetService tests

--- test/unit/edu/usf/RuleChains/RuleSetServiceTests.groovy
+++ test/unit/edu/usf/RuleChains/RuleSetServiceTests.groovy
@@ -9,9 +9,35 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(RuleSetService)
+@Mock([Rule,SQLQuery,RuleSet,ChainServiceHandler,Chain,Link])
 class RuleSetServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testListRuleSets() {
+        def ruleSetService = new RuleSetService()
+        [
+            new RuleSet(name: "firstRuleSet"),
+            new RuleSet(name: "secondRuleSet")
+        ].each { rs ->
+            rs.isSynced = false
+            rs.save()
+        }
+        def result = ruleSetService.listRuleSets()
+        assert result.ruleSets.size() == 2
+        assert result.ruleSets.find { it.name == "firstRuleSet" }.name == "firstRuleSet"
+        assert result.ruleSets.find { it.name == "secondRuleSet" }.name == "secondRuleSet"
+    }
+
+    void testListRuleSetsPattern() {
+        def ruleSetService = new RuleSetService()
+        [
+            new RuleSet(name: "firstRuleSet"),
+            new RuleSet(name: "secondRuleSet")
+        ].each { rs ->
+            rs.isSynced = false
+            rs.save()
+        }
+        def result = ruleSetService.listRuleSets("^(second).*")
+        assert result.ruleSets.size() == 1
+        assert result.ruleSets.find { it.name == "secondRuleSet" }.name == "secondRuleSet"
     }
 }
