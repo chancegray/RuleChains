c4d3002e3a93d86c8f34ea731a5d075ced09e5f5
James Jones <jamjon3@gmail.com>
2014-02-20 15:59:52 -0500

    #58 Adding ChainService tests

--- test/unit/edu/usf/RuleChains/ConfigServiceTests.groovy
+++ test/unit/edu/usf/RuleChains/ConfigServiceTests.groovy
@@ -9,9 +9,30 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ConfigService)
+@Mock([ChainServiceHandler,Chain,Link,Rule,SQLQuery,RuleSet])
 class ConfigServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    /**
+     * Tests the downloading data method
+     */
+    void testDownloadChainData() {
+        
+    }
+    /**
+     * Tests the uploading data method
+     */
+    void testUploadChainData() {
+        def configService = new ConfigService()
+        configService.ruleSetService = new RuleSetService()
+        configService.chainService = new ChainService()
+        configService.chainServiceHandlerService = new ChainServiceHandlerService()
+        configService.jobService = new JobService()
+        def result = configService.uploadChainData([
+            ruleSets: [
+                [ name: "newRuleSet" ]
+            ]
+        ],false)
+        assert result.status == "complete"
+        assert RuleSet.findByName("newRuleSet")?.name == "newRuleSet"
     }
 }
