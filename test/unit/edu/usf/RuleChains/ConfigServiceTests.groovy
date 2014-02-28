7772d3589ae5ff878b822b7f8460c2f3051c8de1
James Jones <jamjon3@gmail.com>
2014-02-28 09:28:31 -0500

    #58 Adding ConfigService tests

--- test/unit/edu/usf/RuleChains/ConfigServiceTests.groovy
+++ test/unit/edu/usf/RuleChains/ConfigServiceTests.groovy
@@ -16,7 +16,30 @@ class ConfigServiceTests {
      * Tests the downloading data method
      */
     void testDownloadChainData() {
-        
+        def configService = new ConfigService()
+        configService.ruleSetService = new RuleSetService()
+        configService.chainService = new ChainService()
+        configService.chainServiceHandlerService = new ChainServiceHandlerService()
+        configService.jobService = new JobService()
+        def c = new Chain(name: "newChain")
+        c.isSynced = false
+        c.save()
+        def rs = new RuleSet(name: "newRuleSet")
+        rs.isSynced = false
+        rs.save()
+        def sr = new SQLQuery(name: "newRuleName",rule: "")
+        sr.isSynced = false
+        rs.addToRules(sr)
+        rs.save()
+        def l = new Link(rule: sr,sequenceNumber: 1)
+        l.isSynced = false
+        c.addToLinks(l)
+        c.save()
+        def csh = new ChainServiceHandler(name: "newChainServiceHandler",chain: c)
+        csh.isSynced = false
+        csh.save()
+        def result = configService.downloadChainData()
+        assert result.ruleSets.find { it.name == "newRuleSet" }?.name == "newRuleSet"
     }
     /**
      * Tests the uploading data method
