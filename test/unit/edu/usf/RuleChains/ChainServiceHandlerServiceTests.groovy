cc6fb5da13c5144e7594cde037d45a941ad7b82c
James Jones <jamjon3@gmail.com>
2014-03-03 14:14:25 -0500

    #58 Adding ChainServiceHandlerService tests.

--- test/unit/edu/usf/RuleChains/ChainServiceHandlerServiceTests.groovy
+++ test/unit/edu/usf/RuleChains/ChainServiceHandlerServiceTests.groovy
@@ -94,4 +94,38 @@ class ChainServiceHandlerServiceTests {
         def result = chainServiceHandlerService.listChainServiceHandlers()
         assert result.chainServiceHandlers.size() == 3        
     }
+    /**
+     * Tests the return of a list of ChainServiceHandler objects with an with option matching filter
+     * 
+     * @see    ChainServiceHandler
+     */   
+    void testListChainServiceHandlersPattern() {
+        def chainServiceHandlerService = new ChainServiceHandlerService()
+        def c = new Chain([name: "newChain"])
+        c.isSynced = false
+        c.save()
+        def rs = new RuleSet(name: "newRuleSet")
+        rs.isSynced = false
+        rs.save()
+        // def sr = new SQLQuery(name: "newRuleName",rule: "SELECT :mykey as testKey FROM DUAL")
+        def sr = new Groovy(name: "newRuleName",rule: "return [ [ testKey: row.mykey ] ]")
+        sr.isSynced = false
+        rs.addToRules(sr)
+        rs.save()         
+        def l = new Link(rule: sr,sequenceNumber: 1,executeEnum: ExecuteEnum.EXECUTE_USING_ROW,resultEnum: ResultEnum.ROW,linkEnum: LinkEnum.NONE,sourceName: "testSource")
+        l.isSynced = false
+        c.addToLinks(l)
+        c.save()
+        l.save()        
+        [
+            new ChainServiceHandler(name: "1stChainHandler",chain: c,method: MethodEnum.GET),
+            new ChainServiceHandler(name: "2ndChainHandler",chain: c,method: MethodEnum.GET),
+            new ChainServiceHandler(name: "3rdChainHandler",chain: c,method: MethodEnum.GET)
+        ].each { csh ->
+            csh.isSynced = false
+            csh.save()
+        }
+        def result = chainServiceHandlerService.listChainServiceHandlers("^(2nd).*")
+        assert result.chainServiceHandlers.first().name == "2ndChainHandler"                
+    }
 }
