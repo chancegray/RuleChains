d828ba06dde0ac798c0a47bfda6d40254907c434
James Jones <jamjon3@gmail.com>
2014-03-05 15:31:20 -0500

    #58 Adding Chain domain class tests.

--- test/unit/edu/usf/RuleChains/ChainTests.groovy
+++ test/unit/edu/usf/RuleChains/ChainTests.groovy
@@ -9,9 +9,19 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Chain)
+@Mock([Rule,RuleSet,ChainServiceHandler])
 class ChainTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testNewChain() {
+        mockDomain(Chain)
+        assert ([name: 'testChain'] as Chain).validate()
+    }
+    
+    void testChainName() {
+        mockDomain(Chain)
+        def newChain = new Chain([name: 'Bad Chain Name'])
+        assert newChain.validate() == false
+        assert newChain.errors.hasFieldErrors("name")
+        assert newChain.errors.getFieldError("name").rejectedValue == 'Bad Chain Name'
     }
 }
