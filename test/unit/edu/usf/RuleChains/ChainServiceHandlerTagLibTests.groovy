26a2170c663943a2c6b0dd3edaaac92bbbfd1d8b
James Jones <jamjon3@gmail.com>
2014-03-05 13:14:27 -0500

    #58 Added some comments

--- test/unit/edu/usf/RuleChains/ChainServiceHandlerTagLibTests.groovy
+++ test/unit/edu/usf/RuleChains/ChainServiceHandlerTagLibTests.groovy
@@ -6,12 +6,21 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
+ * ChainServiceHandlerTagLibTest is unit test for the taglib ChainServiceHandlerTagLib.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
  */
 @TestFor(ChainServiceHandlerTagLib)
 @Mock([ChainServiceHandlerService])
 class ChainServiceHandlerTagLibTests {
-
+    /**
+     * Provides testing for a GSP method to produce a HTML select element of available chain service handlers.
+     * 
+     */ 
     void testChainServiceHandlerSelect() {
         def chainServiceHandlerService = mockFor(ChainServiceHandlerService)
         chainServiceHandlerService.demand.listChainServiceHandlers{-> 
