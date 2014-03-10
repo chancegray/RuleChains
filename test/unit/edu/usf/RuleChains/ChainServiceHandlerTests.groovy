894c930af58c8b6abc5e381813d284fa0c408f4b
James Jones <jamjon3@gmail.com>
2014-03-10 12:29:22 -0400

    #58 Adding ChainServiceHandler domain class tests.

--- test/unit/edu/usf/RuleChains/ChainServiceHandlerTests.groovy
+++ test/unit/edu/usf/RuleChains/ChainServiceHandlerTests.groovy
@@ -6,12 +6,34 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
+ * Testing ChainServiceHandler domain class exposing rule chaings to incoming REST requests
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(ChainServiceHandler)
+@Mock([Rule,RuleSet,Chain])
 class ChainServiceHandlerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    /**
+     * Testing the name validation
+     */
+    void testNewChainServiceHandler() {
+        mockDomain(ChainServiceHandler)
+        def c = new Chain(name: 'testChain')
+        assert (new ChainServiceHandler([name: 'testChainServiceHandler',chain: c])).validate()
+    }
+    /**
+     * Testing bad name's
+     */
+    void testChainServiceHandlerName() {
+        mockDomain(ChainServiceHandler)
+        def newChain = new ChainServiceHandler([name: 'Bad Chain Name'])
+        assert newChain.validate() == false
+        assert newChain.errors.hasFieldErrors("name")
+        assert newChain.errors.getFieldError("name").rejectedValue == 'Bad Chain Name'
     }
 }
