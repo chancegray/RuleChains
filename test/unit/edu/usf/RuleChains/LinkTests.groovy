c118f6dc90911a111b1293cda6b7b60ae1676f47
James Jones <jamjon3@gmail.com>
2014-03-10 15:28:35 -0400

    #58 Adding Link domain class tests.

--- test/unit/edu/usf/RuleChains/LinkTests.groovy
+++ test/unit/edu/usf/RuleChains/LinkTests.groovy
@@ -9,9 +9,28 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Link)
+@Mock([Rule,SQLQuery,RuleSet,Chain,ChainServiceHandler,Link])
 class LinkTests {
 
-    void testSomething() {
-       fail "Implement me"
+    /**
+     * Testing the name validation
+     */
+    void testNewLink() {
+        mockDomain(Link)
+        def c = new Chain(name: 'testChain')
+        c.isSynced = false
+        c.save()
+        def rs = new RuleSet(name: "newRuleSet")
+        rs.isSynced = false
+        rs.save()
+        def sr = new SQLQuery(name: "newRuleName",rule: "")
+        sr.isSynced = false
+        rs.addToRules(sr)
+        rs.save()
+        def l = new Link(rule: sr,sequenceNumber: 1,sourceName: "testSource")
+        l.isSynced = false
+        c.addToLinks(l)
+        c.save()
+        assert l.validate()
     }
 }
