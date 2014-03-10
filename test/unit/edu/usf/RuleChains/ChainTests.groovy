76d1a38719b768d59cf1f3c5c7a99a7de703e84b
James Jones <jamjon3@gmail.com>
2014-03-10 11:25:29 -0400

    #58 Added some comments

--- test/unit/edu/usf/RuleChains/ChainTests.groovy
+++ test/unit/edu/usf/RuleChains/ChainTests.groovy
@@ -6,17 +6,28 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
+ * Testing Chain domain class being the sequencing object for processing
+ * a sequence of rules.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Chain)
 @Mock([Rule,RuleSet,ChainServiceHandler])
 class ChainTests {
-
+    /**
+     * Testing the name validation
+     */
     void testNewChain() {
         mockDomain(Chain)
         assert ([name: 'testChain'] as Chain).validate()
     }
-    
+    /**
+     * Testing bad name's
+     */
     void testChainName() {
         mockDomain(Chain)
         def newChain = new Chain([name: 'Bad Chain Name'])
