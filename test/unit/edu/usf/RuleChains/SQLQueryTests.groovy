412a3c3d0a46027c89c00d3f271e65ff246a1f83
James Jones <jamjon3@gmail.com>
2014-03-12 12:29:40 -0400

    #58 Adding SQLQuery domain class tests.

--- test/unit/edu/usf/RuleChains/SQLQueryTests.groovy
+++ test/unit/edu/usf/RuleChains/SQLQueryTests.groovy
@@ -6,12 +6,34 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
+ * Testing SQLQuery domain class
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(SQLQuery)
+@Mock([Rule,RuleSet,Chain,ChainServiceHandler])
 class SQLQueryTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testNewSQLQuery() {
+        mockDomain(SQLQuery)
+        def rs = new RuleSet(name: "newRuleSet")
+        rs.isSynced = false
+        rs.save()
+        def r = new SQLQuery(name: 'testRule',ruleSet: rs)
+        assert r.validate()
+    }
+    /**
+     * Testing a missing source name
+     */
+    void testSQLRuleMissingRuleSet() {
+        mockDomain(Link)
+        def r = new SQLQuery(name: 'testRule')
+        assert r.validate() == false
+        assert r.errors.hasFieldErrors("ruleSet")
+        assert r.errors.getFieldError("ruleSet").rejectedValue == null
     }
 }
