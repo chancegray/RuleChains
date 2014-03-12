d268fa1b7d01ff0247f452e1fe1fdb0b80283985
James Jones <jamjon3@gmail.com>
2014-03-12 14:38:35 -0400

    #58 Adding StoredProcedureQuery domain class tests.

--- test/unit/edu/usf/RuleChains/StoredProcedureQueryTests.groovy
+++ test/unit/edu/usf/RuleChains/StoredProcedureQueryTests.groovy
@@ -6,12 +6,36 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
+ * Testing StoredProcedureQuery domain class
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(StoredProcedureQuery)
+@Mock([Rule,RuleSet,Chain,ChainServiceHandler])
 class StoredProcedureQueryTests {
-
-    void testSomething() {
-       fail "Implement me"
+    /**
+     * Testing creating a new SQLQuery
+     */
+    void testNewStoredProcedureQuery() {
+        mockDomain(StoredProcedureQuery)
+        def rs = new RuleSet(name: "newRuleSet")
+        rs.isSynced = false
+        rs.save()
+        def r = new StoredProcedureQuery(name: 'testRule',ruleSet: rs)
+        assert r.validate()
+    }
+    /**
+     * Testing a missing ruleset
+     */
+    void testStoredProcedureRuleMissingRuleSet() {
+        mockDomain(StoredProcedureQuery)
+        def r = new StoredProcedureQuery(name: 'testRule')
+        assert r.validate() == false
+        assert r.errors.hasFieldErrors("ruleSet")
+        assert r.errors.getFieldError("ruleSet").rejectedValue == null
     }
 }
