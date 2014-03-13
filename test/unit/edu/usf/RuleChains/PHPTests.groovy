8b4fe055bafa42826f90b44b47d2ac3512014417
James Jones <jamjon3@gmail.com>
2014-03-13 11:57:20 -0400

    #58 Adding PHP domain class tests.

--- test/unit/edu/usf/RuleChains/PHPTests.groovy
+++ test/unit/edu/usf/RuleChains/PHPTests.groovy
@@ -6,12 +6,36 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
+ * Testing PHP domain class
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ * 
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(PHP)
+@Mock([Rule,RuleSet,Chain,ChainServiceHandler])
 class PHPTests {
-
-    void testSomething() {
-       fail "Implement me"
+    /**
+     * Testing creating a new PHP
+     */
+    void testNewPHP() {
+        mockDomain(PHP)
+        def rs = new RuleSet(name: "newRuleSet")
+        rs.isSynced = false
+        rs.save()
+        def r = new PHP(name: 'testRule',ruleSet: rs)
+        assert r.validate()
+    }
+    /**
+     * Testing a missing ruleset
+     */
+    void testPHPRuleMissingRuleSet() {
+        mockDomain(PHP)
+        def r = new PHP(name: 'testRule')
+        assert r.validate() == false
+        assert r.errors.hasFieldErrors("ruleSet")
+        assert r.errors.getFieldError("ruleSet").rejectedValue == null
     }
 }
